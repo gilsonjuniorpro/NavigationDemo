3611d8d83fcb10d4010ee5a09d07a943db57f544
Gilson Junior <gilsonjuniorpro@gmail.com>
2019-01-17 09:15:41 -0500

    Initial commit

---
+++ app/src/main/java/com/navigationdemo/ca/NameFragment.kt
@@ -0,0 +1,29 @@
+package com.navigationdemo.ca
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_name.*
+
+
+class NameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvName.text = arguments?.getString("nameArg")
+
+    }
+}
